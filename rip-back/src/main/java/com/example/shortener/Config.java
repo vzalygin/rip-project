9eb2c77f8d2ce7b819a8fa82d29025695c87866d
v.zalygin <v.zalygin@tbank.ru>
2024-12-17 12:36:26 +0300

    fix cors

--- rip-back/src/main/java/com/example/shortener/Config.java
+++ rip-back/src/main/java/com/example/shortener/Config.java
@@ -1,16 +1,25 @@
 package com.example.shortener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "${springdoc.swagger-ui.server}")
+        }
+)
 @Configuration
 public class Config {
     @Bean
@@ -32,5 +41,15 @@ public class Config {
     @Bean
     MessageDigest messageDigest() throws NoSuchAlgorithmException {
         return MessageDigest.getInstance("SHA-256");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
